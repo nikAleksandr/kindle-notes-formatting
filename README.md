e8c20f71a6c0c341935811fad4d9eca9ca314e23
Nick Lyell <njlyell@gmail.com>
2021-04-01 18:47:26 -0700

    Updates

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ From your kindle device, find MyClippings.txt and drop it into the root folder.
 
 Then fire up a python http.server in the root directory and open the page in a browser.
 
-Notes should be condensed and sorted alphabetically by book for easy copying and pasting wherever you like.
+Notes should be condensed and sorted by date by book for easy copying and pasting wherever you like.
--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var notes,
-	clips,
-	clipsObj = [];
+	oldClips,
+	newClips,
+	oldClipsObj,
+	newClipsObj;
 
 function findBetween(text, start, end, charShift){
 	if(isNaN(start)){
@@ -25,98 +27,130 @@ function sortByProperty(property){
    }  
 }
 
-function getData(){
-	$.get('My Clippings.txt', function(data) {
-	    clips = data.replace(/"/g, "'");
-	    //clips = '{"' + clips.replace(/==========/g, '"}{"') + '""}';
+function searchObj (obj, query) {
 
-	    clips = clips.split("==========");
-	})
-	return;
+    for (var key in obj) {
+        var value = obj[key];
+
+        if (typeof value === 'object') {
+            searchObj(value, query);
+        }
+
+        if (value === query) {
+            //console.log('property=' + key + ' value=' + value);
+            return true;
+        }
+
+    }
+
+}
+
+function getData(fileLoc){	
+    var clips;
+    
+    $.ajax({
+        url: fileLoc,
+        success: function (data) {
+            clips = data.replace(/"/g, "'");
+
+			clips = clips.split("==========");
+        },
+        async: false
+    });
+
+    return clips;
 };
 
-function dataToObject(){
-	setTimeout(() => {
-		//separate each note out into indivudal note objects
-		for(i=0; i <clips.length; i++){
-			var j = (i == 0 | i == clips.length-1) ? 0 : 1;
-			//console.log (i + " : " + j);
+function dataToObject(clips){
+	var clipsObj = [];
+	//console.log(clips);
+	//separate each note out into indivudal note objects
+	for(i=0; i <clips.length; i++){
+		var j = (i == 0 | i == clips.length-1) ? 0 : 1;
+		//console.log (i + " : " + j);
 
 
-			clipsObj[i] = {};
-			clipsObj[i].full = clips[i];
-			//clipsObj[i].book = clips[i].substr(0, clips[i].search("\r\n"));
-			clipsObj[i].arr = clips[i].split("\r\n"); 
-			clipsObj[i].book = clipsObj[i].arr[j];
-			
-			//drop empty highlights
-			if(clipsObj[i].arr.length<3){
-				if (i==clips.length-1) break;
-				clipsObj.splice(i,1);
-				i--;
-				console.log("dropped:" + i + clipsObj[i].arr);
-				continue;
-			}
-
-			//Page and Location properties
-			if(clipsObj[i].arr[j+1].search("page")>0)
-			{
-				clipsObj[i].page = findBetween(clipsObj[i].arr[j+1], "page ", "Added on", -3);
-			}
-			if(clipsObj[i].arr[j+1].search("Location")>0){
-				clipsObj[i].location = findBetween(clipsObj[i].arr[j+1], "Location ", "Added on", -3);
-				//in case it has a location AND a page
-				if(clipsObj[i].arr[j+1].search("page")>0)
-		    	{
-		    		clipsObj[i].page = findBetween(clipsObj[i].arr[j+1], "page ", "Location", -3);
-		    	}
-			}
-			if(clipsObj[i].hasOwnProperty('location')){
-				clipsObj[i].locationStart = Number(findBetween(clipsObj[i].location, 0, "-", 0));
-				clipsObj[i].locationEnd = Number(findBetween(clipsObj[i].location, "-", null, 0));
-			} else {
-				console.log(clipsObj[i]);
-				clipsObj[i].locationStart = Number(findBetween(clipsObj[i].page, 0, "-", 0));
-				clipsObj[i].locationEnd = Number(findBetween(clipsObj[i].page, "-", null, 0));
-			}
-
-			clipsObj[i].highlight = false;
-			if(clipsObj[i].arr[j+1].search("Highlight")>0)
-			{
-				clipsObj[i].highlight = true;
-			}
-			clipsObj[i].date = findBetween(clipsObj[i].arr[j+1], "Added on ",null,0);
-			clipsObj[i].note = clipsObj[i].arr[j+3];
+		clipsObj[i] = {};
+		clipsObj[i].full = clips[i];
+		//clipsObj[i].book = clips[i].substr(0, clips[i].search("\r\n"));
+		clipsObj[i].arr = clips[i].split("\r\n"); 
+		clipsObj[i].book = clipsObj[i].arr[j];
+		
+		//drop empty highlights
+		if(clipsObj[i].arr.length<3){
+			if (i==clips.length-1) break;
+			clipsObj.splice(i,1);
+			i--;
+			console.log("dropped:" + i + clipsObj[i].arr);
+			continue;
 		}
-		//console.log(clipsObj);
-		return;
-	}, 2000);
+
+		//Page and Location properties
+		if(clipsObj[i].arr[j+1].search("page")>0)
+		{
+			clipsObj[i].page = findBetween(clipsObj[i].arr[j+1], "page ", "Added on", -3);
+		}
+		if(clipsObj[i].arr[j+1].search("Location")>0){
+			clipsObj[i].location = findBetween(clipsObj[i].arr[j+1], "Location ", "Added on", -3);
+			//in case it has a location AND a page
+			if(clipsObj[i].arr[j+1].search("page")>0)
+	    	{
+	    		clipsObj[i].page = findBetween(clipsObj[i].arr[j+1], "page ", "Location", -3);
+	    	}
+		}
+		if(clipsObj[i].hasOwnProperty('location')){
+			clipsObj[i].locationStart = Number(findBetween(clipsObj[i].location, 0, "-", 0));
+			clipsObj[i].locationEnd = Number(findBetween(clipsObj[i].location, "-", null, 0));
+		} else {
+			//console.log(clipsObj[i]);
+			clipsObj[i].locationStart = Number(findBetween(clipsObj[i].page, 0, "-", 0));
+			clipsObj[i].locationEnd = Number(findBetween(clipsObj[i].page, "-", null, 0));
+		}
+
+		clipsObj[i].highlight = false;
+		if(clipsObj[i].arr[j+1].search("Highlight")>0)
+		{
+			clipsObj[i].highlight = true;
+		}
+		clipsObj[i].date = moment(findBetween(clipsObj[i].arr[j+1], "Added on ",null,0), "dddd MMMM DD YYYY hh:mm:ss A").toDate();
+		clipsObj[i].note = clipsObj[i].arr[j+3];
+	}
+	//console.log(clipsObj);
+	return clipsObj;
 };	
 	
 //recombine note objects into an array of objects sorted based on book and by locationStart
 function sortAndDisplay(){
-	setTimeout(() => {
-		clipsObj.sort(sortByProperty("locationStart"));
-		clipsObj.sort(sortByProperty("book"));
+	console.log("sortAndDisplay begins!");
+	newClipsObj.sort(sortByProperty("locationStart"));
+	newClipsObj.sort(sortByProperty("book"));
+	//this helps identify which books were more recently read
+	//newClipsObj.sort(sortByProperty("date"));
 
-		console.log(clipsObj.length);
 
-		for(i=0; i<clipsObj.length; i++){
-			console.log("im here" + i);
-			if(i==0 || clipsObj[i].book != clipsObj[i-1].book){
-				$('body').append("<h1>"+ clipsObj[i].book +"</h1>");
-			}
+	oldClipsObj.sort(sortByProperty("locationStart"));
+	oldClipsObj.sort(sortByProperty("book"));
 
-			$('body').append("<p>"+ clipsObj[i].note +"</p>");
 
+	for(i=0; i<newClipsObj.length; i++){
+		//console.log("im here" + i);
+		
+		if(i==0 || newClipsObj[i].book != newClipsObj[i-1].book){
+			$('body').append("<h1>"+ newClipsObj[i].book + ": " + newClipsObj[i].date + "</h1>");
 		}
-	}, 4000);
+
+		$('body').append("<p>"+ newClipsObj[i].note +"</p>");
+
+	}
 };
 
-getData();
-dataToObject();
+newClips = getData('My Clippings.txt');
+oldClips = getData('My Clippings_old.txt');
+newClipsObj = dataToObject(newClips);
+oldClipsObj = dataToObject (oldClips);
 sortAndDisplay();
 
 //need to add handling for own notes - identify and some way to separate them out
 //should add a anchored table of contents at the top
 //should add some of the metadata (maybe at the book level)
+//Need to create a function that makes an array of all the books in oldClipsObj, dedupes them, and then checks each note against the list, and skips any that have books that match a book in the array.
